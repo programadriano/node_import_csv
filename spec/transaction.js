039a9560ff66740826d08059929362955cf8b98f
Thiagtao da Silva Adriano <tadriano@band.com.br>
2018-08-15 17:01:06 -0300

    add test

--- spec/transaction.js
+++ spec/transaction.js
@@ -1,11 +1,16 @@
 
+var expect = require("chai").expect;
 const transaction = require('../bin/transaction.js');
 
 var assert = require('assert');
 describe('transacões bancarias', function () {
 
     it('Valida credito', function () {
-        expect(transaction.calc(100,110,true)).to.equal(115);
+        expect(transaction.calc(100, 100, false)).to.equal(200);
+    });
+
+    it('Valida débito', function () {
+        expect(transaction.calc(1000, 1500, true)).to.equal(2000);
     });
 
 });
